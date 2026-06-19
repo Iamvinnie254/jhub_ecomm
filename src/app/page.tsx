a815ba788f40ed6fa3374e9eb5671d97dc3518cd
stephen <vincentstephen489@gmail.com>
2026-06-19 13:05:36 +0300

    feat: add a hero  section showcasing what to expect in the site and calling the user to get a good grasp of what the business is all about.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Navbar from "./features/landing-page/components/navbar/page"
+import Hero from "./features/landing-page/components/hero-section/page"
+
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
